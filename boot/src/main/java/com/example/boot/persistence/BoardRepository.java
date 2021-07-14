c84b4964676bf763ca6bc7887ff23b3bc73c5b1d
lookhkh <62491156+lookhkh@users.noreply.github.com>
2021-07-14 16:40:10 +0900

    Delete boot directory

--- boot/src/main/java/com/example/boot/persistence/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.boot.persistence;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.boot.domain.Board;
-
-public interface BoardRepository extends CrudRepository<Board, Long> {
-
-}
